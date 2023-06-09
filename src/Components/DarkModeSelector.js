30e012b3a1cb4ae2308543291405af28aa8c254e
AlexMagar <lx.mgr07@gmail.com>
2023-06-09 21:05:15 +1000

    fix the className issue and for into htmlFor

--- src/Components/DarkModeSelector.js
+++ src/Components/DarkModeSelector.js
@@ -3,8 +3,7 @@ import React from 'react'
 export const DarkModeSelector = () => {
   return (
     <div>
-        <input type="checkbox" id="darkMode" />
-        <label for="darkMode" className="dark-mode-toggle">
+        <label htmlFor="darkMode" className="dark-mode-toggle">
             <i className="fa-solid fa-circle-half-stroke"></i>
         </label>
     </div>
